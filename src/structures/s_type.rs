6c842e22591023786240f09610a52e169823e617
kgaft <you@example.com>
2026-08-05 02:58:45 +0300

    fixed structure type

--- src/structures/s_type.rs
+++ src/structures/s_type.rs
@@ -308,9 +308,6 @@ pub fn validate_s_type(target: &dyn StrongType) -> bool {
     let s_type = target.get_s_type();
     let t_1 = s_type.get_type_id();
     let t_2 = target.type_id();
-    let p_meta_t = TypeId::of::<PacketMeta>();
-    let p_ameta_t = TypeId::of::<ArchivedPacketMeta>();
-    println!("t_1: {:?}, t_2: {:?}", p_meta_t, p_ameta_t);
     return t_1 == t_2;
 }
 
